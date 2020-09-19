288e9659c3252dcaed0e00fd4ea018da6097b5b2
Satrio <satrio.10rpl2.2019@gmail.com>
2020-09-19 12:07:01 +0700

    Add Detail movie

---
+++ app/src/main/java/com/example/satrio11rpl022019/DataMovie.java
@@ -0,0 +1,13 @@
+package com.example.satrio11rpl022019;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DataMovie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_movie);
+    }
+}
